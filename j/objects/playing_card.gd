103c7f99bd80405ae64f811e180e1dcd64e772a2
Julia Ahopelto <me@notjulie.dev>
2026-06-19 22:54:19 -0700

    waow cool shaders

---
+++ j/objects/playing_card.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+enum Suit {
+	Spade,
+	Heart,
+	Diamond,
+	Club
+}
+
+@export var face_up: bool = true
+@export var suit: Suit = Suit.Spade
+@export var number: int = 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var back = $"VisibleCard/BackTexture"
+	var face = $"VisibleCard/FaceTexture"
+	var border = $"VisibleCard/BorderTexture"
+	
+	if not face_up:
+		back.region_rect = Rect2(250, 0, 250, 350)
+		face.visible = false
+		border.region_rect = Rect2(54 * 250, 0, 250, 350)
+		return
+
+	
+	var index = number
+	if suit == Suit.Spade:
+		index += 0
+	elif suit == Suit.Heart:
+		index += 13
+	elif suit == Suit.Club:
+		index += 26
+	elif suit == Suit.Diamond:
+		index += 39
+
+	back.region_rect = Rect2(0, 0, 250, 350)
+	face.region_rect = Rect2(index * 250, 0, 250, 350)
+	border.region_rect = Rect2(55 * 250, 0, 250, 350)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
