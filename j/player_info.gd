a148d5f5691b7f63edb490765d5a487d582c5827
maxrchung <maxrchung@gmail.com>
2026-06-21 11:39:35 -0700

    simplify player name

--- j/player_info.gd
+++ j/player_info.gd
@@ -6,7 +6,7 @@ var score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Name.text = "Name: " + player_name
+	$Name.text = player_name
 	
 	if int(card_count) == 0:
 		$CardCount.text = "Cards: -"
